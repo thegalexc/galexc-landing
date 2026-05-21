262506fdf928c587a8dd2ccbb56ad58140c40d6e
Gilman Callsen <gilman.callsen@rhoimpact.com>
2026-05-21 17:03:34 -0400

    feat: simplify landing page and enable waitlist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://galexc.net',
   output: 'server',
   adapter: cloudflare(),
-  integrations: [
-    sitemap({
-      filter: (page) => !page.endsWith('/admin'),
-    }),
-  ],
   vite: {
     plugins: [tailwindcss()],
   },
